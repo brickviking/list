6aad2671f6fc0877dec19cd7dc6ceeebab1a0f07
brickviking <brickviking@gmail.com>
2022-01-21 23:36:16 +1300

    Updated ChangeLog and version

--- src/list.h
+++ src/list.h
@@ -1,7 +1,7 @@
 /* Now called list.h instead of hexdump.h */
 /* Global defines */
 #define LISTVERSION 0
-#define LISTMINOR 31
+#define LISTMINOR 32
 #define LISTMAJOR 7
 /* Had to include this one, since it's disappeared from 2.2 linux kernel or something... */
 #ifndef PATH_MAX
