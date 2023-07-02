6d24d01bf5438ee6726ac17a31e32ea8e76ce5cb
brickviking <brickviking@gmail.com>
2023-07-02 12:38:48 +1200

    Revision bump to go with the Usage() tweak from last commit.

--- src/list.h
+++ src/list.h
@@ -3,7 +3,7 @@
 #define LISTVERSION 0
 #define LISTMINOR 37
 #define LISTMAJOR 7
-#define LISTREV 0
+#define LISTREV 1
 /* Had to include this one, since it's disappeared from 2.2 linux kernel or something... */
 #ifndef PATH_MAX
 #define PATH_MAX  4095
